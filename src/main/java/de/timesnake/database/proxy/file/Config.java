a24c0d1f3915a30e6563f53e814b77d2c5537f0d
funzter <funzter@mail.timesnake.de>
2022-07-12 20:49:01 +0200

    update to velocity

--- src/main/java/de/timesnake/database/proxy/file/Config.java
+++ src/main/java/de/timesnake/database/proxy/file/Config.java
@@ -1,21 +1,23 @@
 package de.timesnake.database.proxy.file;
 
+import com.moandjiezana.toml.Toml;
+import com.moandjiezana.toml.TomlWriter;
 import de.timesnake.database.core.file.DatabaseConfig;
 import de.timesnake.database.core.file.DatabaseNotConfiguredException;
-import net.md_5.bungee.config.Configuration;
-import net.md_5.bungee.config.ConfigurationProvider;
-import net.md_5.bungee.config.YamlConfiguration;
 
 import java.io.File;
 import java.io.IOException;
 
 public class Config implements DatabaseConfig {
 
-    private final File configFile = new File("plugins/database/config.yml");
-    private Configuration config;
+    protected final File configFile;
+    protected Toml config;
+    protected TomlWriter writer;
 
-    public void onEnable() {
+    public Config() {
+        this.configFile = new File("plugins/database/config.toml");
 
+        //directory creation
         File dir = new File("plugins/database");
         if (!dir.exists()) {
             dir.mkdir();
@@ -26,69 +28,19 @@ public class Config implements DatabaseConfig {
                 configFile.createNewFile();
             } catch (IOException e) {
                 e.printStackTrace();
-                return;
             }
-
-            this.load();
-
-            config.set("database.servers.url", "jdbc:mysql://localhost:3306/servers");
-            config.set("database.servers.name", "servers");
-
-            config.set("database.users.url", "jdbc:mysql://localhost:3306/users");
-            config.set("database.users.name", "users");
-
-            config.set("database.permissions.url", "jdbc:mysql://localhost:3306/permissions");
-            config.set("database.permissions.name", "permissions");
-
-            config.set("database.groups.url", "jdbc:mysql://localhost:3306/groups");
-            config.set("database.groups.name", "groups");
-
-            config.set("database.games_info.url", "jdbc:mysql://localhost:3306/games_info");
-            config.set("database.games_info.name", "game_info");
-
-            config.set("database.games_teams.url", "jdbc:mysql://localhost:3306/games_teams");
-            config.set("database.games_teams.name", "game_teams");
-
-            config.set("database.games_maps.url", "jdbc:mysql://localhost:3306/games_maps");
-            config.set("database.games_maps.name", "game_maps");
-
-            config.set("database.games_kits.url", "jdbc:mysql://localhost:3306/games_kits");
-            config.set("database.games_kits.name", "game_kits");
-
-            config.set("database.games_lounges.url", "jdbc:mysql://localhost:3306/games_lounges");
-            config.set("database.games_lounges.name", "game_lounges");
-
-            config.set("database.support.url", "jdbc:mysql://localhost:3306/support");
-            config.set("database.support.name", "support");
-
-            config.set("database.survival.url", "jdbc:mysql://localhost:3306/survival");
-            config.set("database.survival.name", "survival");
-
-            config.set("database.decorations.url", "jdbc:mysql://localhost:3306/decorations");
-            config.set("database.decorations.name", "decorations");
-
-            config.set("database.url", "jdbc:mysql://localhost:3306/mysql");
-            config.set("database.user", "root");
-            config.set("database.password", "password");
-
-            this.save();
         }
+        this.config = new Toml();
+
+        this.load();
     }
 
     public void load() {
-        try {
-            config = ConfigurationProvider.getProvider(YamlConfiguration.class).load(configFile);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        config.read(configFile);
     }
 
     public void save() {
-        try {
-            ConfigurationProvider.getProvider(YamlConfiguration.class).save(config, configFile);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        writer.write(configFile);
     }
 
     @Override
