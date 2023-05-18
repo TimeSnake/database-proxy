7b4867e23bab427908b8d363754d0eb108b05b49
funzter <funzter@mail.timesnake.de>
2023-05-18 11:27:34 +0200

    reindented files with two spaces

--- src/main/java/de/timesnake/database/proxy/main/DatabaseProxy.java
+++ src/main/java/de/timesnake/database/proxy/main/DatabaseProxy.java
@@ -16,41 +16,41 @@ import de.timesnake.database.util.Database;
 import java.util.logging.Logger;
 
 @Plugin(id = "database-proxy", name = "DatabaseProxy", version = "1.0-SNAPSHOT",
-        url = "https://git.timesnake.de", authors = {"MarkusNils"},
-        dependencies = {
-                @Dependency(id = "channel-proxy")
-        })
+    url = "https://git.timesnake.de", authors = {"MarkusNils"},
+    dependencies = {
+        @Dependency(id = "channel-proxy")
+    })
 public class DatabaseProxy {
 
-    public static ProxyServer getServer() {
-        return server;
+  public static ProxyServer getServer() {
+    return server;
+  }
+
+  public static Logger getLogger() {
+    return logger;
+  }
+
+  private static ProxyServer server;
+  private static Logger logger;
+
+  @Inject
+  public DatabaseProxy(ProxyServer server, Logger logger) {
+    DatabaseProxy.server = server;
+    DatabaseProxy.logger = logger;
+  }
+
+  @Subscribe
+  public void onProxyInitialization(ProxyInitializeEvent event) {
+    Config config = new Config();
+    try {
+      Database.getInstance().connect(config);
+    } catch (DatabaseNotConfiguredException e) {
+      logger.warning(e.getMessage());
     }
 
-    public static Logger getLogger() {
-        return logger;
-    }
+    Database.getInstance().createTables();
 
-    private static ProxyServer server;
-    private static Logger logger;
+    logger.info("[Database] Databases loaded successfully!");
 
-    @Inject
-    public DatabaseProxy(ProxyServer server, Logger logger) {
-        DatabaseProxy.server = server;
-        DatabaseProxy.logger = logger;
-    }
-
-    @Subscribe
-    public void onProxyInitialization(ProxyInitializeEvent event) {
-        Config config = new Config();
-        try {
-            Database.getInstance().connect(config);
-        } catch (DatabaseNotConfiguredException e) {
-            logger.warning(e.getMessage());
-        }
-
-        Database.getInstance().createTables();
-
-        logger.info("[Database] Databases loaded successfully!");
-
-    }
+  }
 }
