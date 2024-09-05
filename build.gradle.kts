9f3758b336e0d31d8f6f146ebf3bb831c3c7bd1d
funzter <cavehive@protonmail.com>
2024-09-06 00:53:24 +0200

    change gradle dsl to kotlin

---
+++ build.gradle.kts
@@ -0,0 +1,81 @@
+plugins {
+    id("java")
+    id("java-base")
+    id("java-library")
+    id("maven-publish")
+    id("com.github.johnrengelman.shadow") version "8.1.1"
+}
+
+
+group = "de.timesnake"
+version = "3.0.0"
+var projectId = 49
+
+repositories {
+    mavenLocal()
+    mavenCentral()
+    maven {
+        url = uri("https://repo.papermc.io/repository/maven-public/")
+    }
+    maven {
+        url = uri("https://git.timesnake.de/api/v4/groups/7/-/packages/maven")
+        name = "timesnake"
+        credentials(PasswordCredentials::class)
+    }
+}
+
+dependencies {
+    implementation("de.timesnake:database-api:3.+")
+
+    compileOnly("com.velocitypowered:velocity-api:3.1.1")
+    annotationProcessor("com.velocitypowered:velocity-api:3.1.1")
+}
+
+configurations.configureEach {
+    resolutionStrategy.dependencySubstitution {
+        if (project.parent != null) {
+            substitute(module("de.timesnake:database-api")).using(project(":database:database-api"))
+        }
+    }
+}
+
+tasks.register<Copy>("exportAsPlugin") {
+    from(layout.buildDirectory.file("libs/${project.name}-${project.version}-all.jar"))
+    into(findProperty("timesnakePluginDir") ?: "")
+
+    dependsOn("shadowJar")
+}
+
+tasks.withType<PublishToMavenRepository> {
+    finalizedBy("shadowJar")
+}
+
+publishing {
+    repositories {
+        maven {
+            url = uri("https://git.timesnake.de/api/v4/projects/$projectId/packages/maven")
+            name = "timesnake"
+            credentials(PasswordCredentials::class)
+        }
+    }
+
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["java"])
+        }
+    }
+}
+
+tasks {
+    compileJava {
+        options.encoding = "UTF-8"
+        options.release = 21
+    }
+}
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+    withSourcesJar()
+}
